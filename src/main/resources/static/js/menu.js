2364d50c36c93ba9b49b28b9b137c5f6cf2553c9
mihan <mihan-arbuzov@yandex.ru>
2022-09-07 10:56:27 +0400

    Создал функционал поиска

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -22,6 +22,241 @@
         searchMenu.classList.toggle('open');
     });
 
+    // функционал поиска
+    const input = document.querySelector("#elastic");
+    const btn = document.querySelector(".btnSearch");
+    const divs = document.querySelectorAll(".hide");
+    const notdata = document.querySelector(".notfound");
+    const data = document.querySelector(".notf");
+    btn.addEventListener("click",function(){
+        let val = input.value.trim();
+        if(val != ''){
+            data.textContent = val;
+            notdata.classList.add('visible');
+            input.value='';
+            for(let div of divs){
+                const childDiv = div.firstElementChild;
+                const items = childDiv.firstElementChild;
+                const itemBody = items.lastElementChild;
+                const itemLink = itemBody.firstElementChild;
+                const a = itemLink.firstElementChild;
+                if(a.innerText.search(val) == -1){
+                    switch(a.textContent){
+                        case 'Процессор':{
+                            if(val =='процессор'| val == 'Intel'| val == 'AMD'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Материнская плата':{
+                            if(val =='материнская плата'| val == 'материнская'| val == 'материнка'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Кулер для процессора':{
+                            if(val =='кулер'| val == 'кулер для процессора'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Блок питания':{
+                            if(val =='блок питания'| val == 'блок'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Видеокарта':{
+                            if(val =='видеокарта'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Жёсткий диск':{
+                            if(val =='жёсткий диск'| val == 'жесткий диск'| val == 'винчестер' | val == 'hdd'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Твердотельный накопитель':{
+                            if(val =='твердотельный накопитель'| val == 'SSD'| val == 'ssd'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Оперативная память':{
+                            if(val =='oперативная память'| val == 'оперативка'| val == 'модули памяти'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Кулер для корпуса':{
+                            if(val =='кулер для корпуса'| val == 'кулер'| val == 'вентилятор'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Корпус':{
+                            if(val =='корпус'| val == 'системник'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Монитор':{
+                            if(val =='монитор'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Клавиатура':{
+                            if(val =='клавиатура'| val == 'клава' | val == 'keyboard'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Компьютерная мышь':{
+                            if(val =='компьютерная мышь'| val == 'мышка' | val == 'mouse'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'МФУ':{
+                            if(val =='мфу'| val == 'сканер' | val == 'принтер'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case 'Антивирусная программа':{
+                            if(val =='антивирусная программа'| val == 'антивирус'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case'Офисная программа':{
+                            if(val =='офисная программа'| val == 'Microsoft Office' | val == 'office'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case'Операционная система':{
+                            if(val =='операционная система'| val == 'операционка'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                        case'Программное обеспечение':{
+                            if(val =='программное обеспечение'| val == 'софт' | val == 'Софт'){
+                                div.classList.add('visi');
+                                notdata.classList.remove('visible');
+                                input.value='';
+                            }
+                            else{
+                                div.classList.remove('visi');
+                            }
+                        }
+                            break;
+                    }
+                }
+                else{
+                    div.classList.add('visi');
+                    notdata.classList.remove('visible');
+                    input.value='';
+                }
+            }
+        }
+        else{
+            for(let div of divs){
+                div.classList.remove('visi');
+                notdata.classList.remove('visible');
+            }
+        }
+    });
+
     // функции на разные экраны для выпадающего списка
     const dropdown = document.querySelector(".dropdown");
     const mediaQuery = window.matchMedia("(min-width: 790px)");
